b87231f5e1b4f756f0da43a90c49e7d19612e454
MZC <MZC@DESKTOP-3F8449F>
2021-10-22 11:06:25 +0900

    [기능 추가] controller CommandFactory 추가

---
+++ src/controller/CommandFactory.java
@@ -0,0 +1,37 @@
+package controller;
+
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandFactory {
+	private static CommandFactory factory;
+	
+	private Map<String, String> map = new HashMap<String, String>();
+	
+	// constructor
+	private CommandFactory() {}
+	
+	public static CommandFactory getInstance() {
+		if(factory == null) {
+			factory = new CommandFactory();
+		}
+		return factory;
+	}
+	
+	public Command createCommand(String commandURI) throws Exception {
+		String commandClass = map.get(commandURI);
+		if(commandClass == null) {
+			return null;
+		}
+		try {
+			Class<?> cls = Class.forName(commandClass);
+			Constructor<?> constructor = cls.getConstructor(null);
+			Command command = (Command) constructor.newInstance();
+			return command;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
+}
